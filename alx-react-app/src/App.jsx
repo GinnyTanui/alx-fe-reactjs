c0e54d2ef11fd038892f4e0af597898c2b3c71f8
GinnyTanui <tanuiginny@gmail.com>
2024-08-09 14:12:14 +0300

    Initial changes

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,22 @@
+import WelcomeMessage from "./WelcomeMessage.jsx";
+import Header from "./Header.jsx"; 
+import MainContent from "./MainContent.jsx"; 
+import Footer from "./Footer.jsx";
+import UserProfile from "./UserProfile.jsx";
+
+function App() {  
+  return( 
+    <> 
+    <WelcomeMessage/> 
+    <Header/> 
+    <MainContent/> 
+    <Footer/> 
+    <UserProfile name="Alice"  age="25" bio="Loves hiking and photography"/>
+    </>
+
+  )
+ 
+ 
+}
+
+export default App
