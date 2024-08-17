a5413c8de9888c93610edd21750c1402a02f7388
GinnyTanui <tanuiginny@gmail.com>
2024-08-17 12:45:16 +0300

    Changes

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){ 
+    return(
+        <footer>
+            <p style={{fontWeight: 'bold'}}>&copy; 2023 City Lovers</p>
+        </footer>
+    )
+
+} 
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+
+function MainContent(){ 
+    return(
+        <main>
+            <p style={{color: 'aqua', margin: '10px'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+
+} 
+export default MainContent
