dbad72ee93c2f349c10a247d31de1b69c2dcd094
GinnyTanui <162957566+GinnyTanui@users.noreply.github.com>
2024-08-18 18:29:45 +0300

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 function Navbar(){ 
     return(
-        <nav>
+        <nav style={{backgroundColor: 'blue'}}>
             <ul>
                 <li><Link to='./'>Home</Link></li> 
                 <li><Link to='./about'>About</Link></li>  
