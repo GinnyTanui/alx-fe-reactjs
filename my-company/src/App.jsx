e25a55364765e1b5298889db2a866bae54a5ef88
GinnyTanui <tanuiginny@gmail.com>
2024-08-18 18:27:13 +0300

    Changes

---
+++ my-company/src/App.jsx
@@ -0,0 +1,27 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom' 
+import Navbar from './components/Navbar.jsx'
+import Home  from './components/Home.jsx' 
+import About from './components/About.jsx' 
+import Contact from './components/Contact.jsx' 
+import Services from './components/Services.jsx'
+
+function App() {
+ 
+  return (
+    <>  
+    <BrowserRouter>
+    <Navbar/>
+    <Routes> 
+      <Route path='./' element={<Home/>}/> 
+      <Route path='./about' element={<About/>}/>  
+      <Route path='./contact' element={<Contact/>}/> 
+      <Route path='./services' element={<Services/>}/>
+
+    </Routes> 
+    </BrowserRouter>
+    </>
+  )
+   
+}
+
+export default App
