f63f26d76a0e7eb3f4214c6257dbb87cd4ec8327
GinnyTanui <tanuiginny@gmail.com>
2024-09-01 15:01:13 +0300

    Initial

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react" 
 import RegistrationForm from "./components/RegistrationForm" 
-import FormikForm from "./components/formikForm"
+import NewFormikForm from "./components/newFormikForm"
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
      <RegistrationForm/> 
-     <FormikForm/>
+     <NewFormikForm/>
      
     </>
   )
