3fc74ca9da3cf534b225393db13f30dfbb8ae2f1
GinnyTanui <tanuiginny@gmail.com>
2024-09-01 22:40:47 +0300

    changes

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom"; 
 import {isAuthenticated} from '..auth'; 
-
+["useAuth"]
 const ProtectedRoute = ({children}) => {
   if(!isAuthenticated()){
     return<Navigate to="/" replace/>
