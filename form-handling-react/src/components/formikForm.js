c5c69ac5c45aa5cb1be37e8ca639b7ea36097200
GinnyTanui <tanuiginny@gmail.com>
2024-09-01 15:10:22 +0300

    initial

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,6 +1,6 @@
  
 import * as Yup from 'yup';
-{ "Formik", "Form", "Field", "ErrorMessage", "initialValues"}
+{ "Formik", "Form", "Field", "ErrorMessage", "initialValues", "validationSchema"}
 const signUpSchema = Yup.object({
         name: Yup.string().required("Please enter your username"), 
         email: Yup.string().required("Please enter a valid email"),
