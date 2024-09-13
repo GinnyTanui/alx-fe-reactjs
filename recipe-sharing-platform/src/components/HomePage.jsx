1351d7f0081263f2edb913e4fa27dcbecba49ea7
GinnyTanui <tanuiginny@gmail.com>
2024-09-13 10:20:09 +0300

    added styles

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -32,10 +32,11 @@ const HomePage = () => {
 
 
     return(
-        <div>
-            <h1 className="text-3xl text-center italic font-bold text-cyan-600 sm:text-xl">Recipe Sharing</h1>   
-            <ul className=" flex justify-between  " >{data.map((item) => (
-                <div key={item.id} className=" flex flex-col items-start sm:items-center w-full sm:w-1/2  p-4 rounded-lg shadow-lg ">
+        <div> 
+            <h1 className="italic text-red-600 ml-6 hover:underline">Home</h1>
+            <h1 className="text-6xl text-center italic font-bold text-cyan-600 mb-4">Recipe Sharing</h1>   
+            <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 bg-pink-200  " >{data.map((item) => (
+                <div key={item.id} className=" flex flex-col items-start sm:items-center w-full sm:w-1/2  p-4 rounded-lg shadow-md hover:shadow-lg hover:bg-pink-100 transition-all duration-300 ">
                     <h2 className="text-xl font-bold mb-2 sm:text-lg">{item.title}</h2> 
                     <p className="text-gray-600">{item.summary}</p> 
                     <img src={item.imageUrl} alt={item.title} className="w-full sm:w-40 h-32 object cover mb-4 sm:mb-0 sm:mr-4 rounded-lg"/>
