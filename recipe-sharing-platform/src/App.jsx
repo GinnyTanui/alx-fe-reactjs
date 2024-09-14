8aa8c678905c686d9b75827d5cbb44335482579d
GinnyTanui <tanuiginny@gmail.com>
2024-09-14 21:07:01 +0300

    not yet

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,7 @@
 import HomePage from "./components/HomePage"; 
 import RecipeDetail from "./components/RecipeDetail";
-import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
+import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom'; 
+import AddRecipeForm from "./components/AddRecipeForm";
 function App() {
   
 
@@ -9,11 +10,18 @@ function App() {
     <> 
    
      
-     <Router> 
+     <Router>  
+      { <nav>
+        <ul className="p-4 text-purple-500 italic"> 
+          <Link to = '/'>Home</Link> 
+          <Link to ='/add'>Add Recipe</Link>
+        </ul>
+      </nav> }
       
        <Routes> 
         <Route path ='/' element={<HomePage/>}/>
-        <Route path='/recipe/:id' element={<RecipeDetail/>}/>
+        <Route path='/recipe/:id' element={<RecipeDetail/>}/> 
+        <Route path='/add' element={<AddRecipeForm/>}/>
          
        </Routes>
      </Router> 
