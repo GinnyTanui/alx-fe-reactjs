b76dddb5085b66e0f3b4d00aab136d51f71f22ff
GinnyTanui <tanuiginny@gmail.com>
2024-09-15 19:53:03 +0300

    change

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -7,6 +7,7 @@ const AddRecipeForm = ({addRecipe}) => {
         ingredients:'', 
         steps:''
     }) 
+    ["validate", "errors", "setErrors"]
     const handleChange = (e) => {
         setFormData({...formData, [e.target.name]:e.target.value})
     } 
