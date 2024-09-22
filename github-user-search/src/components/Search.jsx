16c7817f401d799bf177399e998bd9b2597dea99
GinnyTanui <tanuiginny@gmail.com>
2024-09-22 12:53:37 +0300

    changes

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -13,10 +13,13 @@ const SearchUsers = () => {
         setError(""); 
         setUserData(null)   
         
-        const username = e.target.elements.username.value;
+        const username = e.target.elements.username.value; 
+        const location = e.target.elements.location.value; 
+        const minRepos = e.target.elements.minRepos.value;
+
 
         try{
-            const data = await fetchUserData(username); 
+            const data = await fetchUserData(username, location, minRepos); 
             console.log(data)
             setUserData(data)
             
@@ -31,14 +34,29 @@ const SearchUsers = () => {
         
    // } 
     return (
-        <div>
-            <form onSubmit={handleSubmit}> 
-                <label>Enter a username</label> 
-                <input type="text" name="username" id="username"  placeholder="Enter github username" /> 
-                <button type="submit">Submit</button> 
+        <div className="container mx-auto px-4 py-7">
+            <form onSubmit={handleSubmit} className="max-w-lg mx-auto p-6 shadow-md rounded-lg"> 
+                <label className="block font-bold mb-2 text-sm">Enter a username</label> 
+                <input type="text" name="username" id="username"  placeholder="Enter github username" />   
+                <label className="block font-bold mb-2 text-sm">Enter Location</label>
+                <input type="text" name="location" id="location" />  
+                <label className="block font-bold mb-2 text-sm">Enter min repos</label>
+                <input type="text" name="minRepos" id="minRepos" />
+                <button type="submit" className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none">Submit</button> 
                 {loading && <p>Loading...</p>} 
-                {error && <p>{error}</p>}  
-                {userData ? (
+                {error && <p>{error}</p>}   
+                {Array.isArray(userData) && userData.length > 0 && (
+       userData.map((user) => (
+           <div key={user.id}> 
+               <h3>Username: {user.login}</h3> 
+               <p>Location: {user.location}</p> 
+               <img src={user.avatar_url} alt={user.login} /> 
+               <p>Public Repositories: {user.public_repos}</p> 
+               <p>Profile: <a href={user.html_url} target="_blank" rel="noopener noreferrer">View profile</a></p>
+           </div>
+       ))
+   )}
+                {/* {userData ? (
                     <div>
                         <h2>Github User Information</h2>  
                         <img src={userData.avatar_url} /> 
@@ -46,7 +64,7 @@ const SearchUsers = () => {
                         <p>Profile: <a href={userData.html_url} target="blank" rel="noopener noreferrer">View profile</a></p>
                     </div>
                 )
-                : <p>Looks like we cannot find the user</p> }
+                : <p>Looks like we cannot find the user</p> } */}
 
             </form>
         </div>
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,14 @@
 import axios from 'axios'; 
 
- export const fetchUserData = async (username) => {  
+ export const fetchUserData = async (username, location, minRepos) => {  
 
     try{ 
-        ["https://api.github.com"]
-        const response = await axios.get(`https//api.github.com/users/${username}`) 
+       // ["https://api.github.com"] 
+       const query = `q=${username ? username : ''}+ ${location ? location: ''} + repos:>${minRepos ? minRepos: 0}` 
+       const response = await axios.get(`https://api.github.com/search/users?${query}`)
+       // const response = await axios.get(`https/api.github.com/users/${username}`) 
         console.log(response.data)
-        return response.data 
+        return response.data.items
         
     }catch(error){
         throw error;
